42e49b9d9142b12866b87382c0ac5b4701d42957
nodir-sulaymonov <s.nodir9494@gmail.com>
2019-07-06 11:17:01 +0500

    money exchange push

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// PLEASE DON'T change function name
+module.exports = function makeExchange(currency) {
+
+    let result = {};
+    let half = 0;
+    let quarters = 0;
+    let dimes = 0;
+    let nickels = 0;
+    let pennies = 0;
+    if (currency <= 0) {
+        return result
+    }
+    while (currency !== 0) {
+        if (currency >= 50) {
+            currency -= 50;
+            half++;
+        } else if (currency >= 25) {
+            currency -= 25;
+            quarters++;
+        } else if (currency >= 10) {
+            currency -= 10;
+            dimes++;
+        } else if (currency >= 5) {
+            currency -= 5;
+            nickels++;
+        } else {
+            currency -= 1;
+            pennies++;
+        }
+    }
+
+    if (half > 0) {
+        result["H"] = half
+    }
+    if (quarters > 0) {
+        result["Q"] = quarters
+    }
+    if (dimes > 0) {
+        result["D"] = dimes
+    }
+    if (nickels > 0) {
+        result["N"] = nickels
+    }
+    if (pennies > 0) {
+        result["P"] = pennies
+    }
+    return result
+
+};
